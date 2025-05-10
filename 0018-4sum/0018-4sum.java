2bb5f2760061f2ebbcabbdb140347830642caa26
Abdul khaliq <99346275+Abdul5786@users.noreply.github.com>
2025-05-10 22:16:37 +0530

    Time: 33 ms (14.22%), Space: 45.6 MB (11.32%) - LeetHub

---
+++ 0018-4sum/0018-4sum.java
@@ -0,0 +1,38 @@
+class Solution {
+    public List<List<Integer>> fourSum(int[] nums, int target) {
+        Arrays.sort(nums); // Sort to use two-pointer technique
+
+        List<List<Integer>> res = new ArrayList<>();
+
+        for (int i = 0; i < nums.length - 3; i++) {
+            if (i > 0 && nums[i] == nums[i - 1]) continue; // Skip duplicates for i
+
+            for (int j = i + 1; j < nums.length - 2; j++) {
+                if (j > i + 1 && nums[j] == nums[j - 1]) continue; // Skip duplicates for j
+
+                int left = j + 1, right = nums.length - 1;
+
+                while (left < right) {
+                 Long sum = 1L *  nums[i] + nums[j] + nums[left] + nums[right];
+
+                    if (sum == target) {
+                        res.add(Arrays.asList(nums[i], nums[j], nums[left], nums[right]));
+
+                        // Skip duplicates for left and right
+                        while (left < right && nums[left] == nums[left + 1]) left++;
+                        while (left < right && nums[right] == nums[right - 1]) right--;
+
+                        left++;
+                        right--;
+                    } else if (sum < target) {
+                        left++;
+                    } else {
+                        right--;
+                    }
+                }
+            }
+        }
+
+        return res;
+    }
+}
