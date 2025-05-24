a6b4aec2c5b61f92c789ecdfbcf083496781514e
Abdul khaliq <99346275+Abdul5786@users.noreply.github.com>
2025-05-25 03:05:37 +0530

    Time: 5 ms (99.9%), Space: 45.1 MB (46.68%) - LeetHub

--- 0532-k-diff-pairs-in-an-array/0532-k-diff-pairs-in-an-array.java
+++ 0532-k-diff-pairs-in-an-array/0532-k-diff-pairs-in-an-array.java
@@ -1,29 +1,63 @@
 class Solution {
     public int findPairs(int[] nums, int k) 
     {
-        HashMap<Integer,Integer> map =  new HashMap<>();
+    //     HashMap<Integer,Integer> map =  new HashMap<>();
 
-        for(int num:nums)
-        {
-            map.put(num,map.getOrDefault(num,0)+1);  //  created frequency array
-        }
+    //     for(int num:nums)
+    //     {
+    //         map.put(num,map.getOrDefault(num,0)+1);  //  created frequency array
+    //     }
 
-        int count=0;
+    //     int count=0;
 
-        for(int key:map.keySet())
-        {
-           if(k>0 && map.containsKey(key+k))
-           {
-            count++;
-           }
+    //     for(int key:map.keySet())
+    //     {
+    //        if(k>0 && map.containsKey(key+k))
+    //        {
+    //         count++;
+    //        }
 
-           else if(k==0 && map.get(key)>1)
-           {
+    //        else if(k==0 && map.get(key)>1)
+    //        {
 
-            count++;
-           }
+    //         count++;
+    //        }
+    //     }
+
+    //     return count;
+    // }
+     
+      int count = 0;
+        int left = 0;
+        int right = 1;
+
+        Arrays.sort(nums);
+
+        while (right < nums.length) {
+            int differ = nums[right] - nums[left];
+
+            if (differ == k) {
+                count++;
+                left++;
+                right++;
+
+                // skip duplicates
+                while (right < nums.length && nums[right] == nums[right - 1]) {
+                    right++;
+                }
+            } else if (differ < k) {
+                right++;
+            } else {
+                left++;
+            }
+
+            if (left == right) {
+                right++;
+            }
         }
 
         return count;
     }
+
+
 }
