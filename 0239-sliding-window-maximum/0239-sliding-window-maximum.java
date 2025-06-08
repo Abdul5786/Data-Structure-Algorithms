8cdba0bb4aa1ee92a67232e7716dd1445a0af318
Abdul khaliq <99346275+Abdul5786@users.noreply.github.com>
2025-06-08 16:41:05 +0530

    Time: 29 ms (85.65%), Space: 59.4 MB (46.52%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.java
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.java
@@ -1,17 +1,31 @@
 class Solution {
-    public int[] maxSlidingWindow(int[] nums, int k) {
-        int n = nums.length;
-        int[] ans = new int[n - k + 1];
+    public int[] maxSlidingWindow(int[] nums, int k) 
+    {
+        int n= nums.length;
+        int ans[] = new int[n-k+1]; // result array 
         Deque<Integer> dq = new ArrayDeque<>();
-        for (int i = 0; i < n; i++) {
-            // Window se bahar wale index hatao
-            if (!dq.isEmpty() && dq.peekFirst() == i - k) dq.pollFirst();
-            // Chhote elements ko piche se hatao
-            while (!dq.isEmpty() && nums[dq.peekLast()] < nums[i]) dq.pollLast();
-            dq.offerLast(i);
-            // Window complete hone pe answer add karo
-            if (i >= k - 1) ans[i - k + 1] = nums[dq.peekFirst()];
-        }
-        return ans;
+
+         for(int i=0;i<n;i++)
+         {
+            while(!dq.isEmpty()&& nums[dq.peekLast()]<=nums[i])
+            {
+                dq.removeLast();
+            }
+
+            dq.addLast(i);
+
+            if(dq.peekFirst()<=i-k)
+            {
+                dq.removeFirst();
+            }
+
+            if(i>=k-1)   // window formed 
+            {
+             ans[i-k+1]=nums[dq.peekFirst()];
+            } 
+         }
+
+      return ans;  
     }
+    
 }
