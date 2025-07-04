06e0f6968b1873e9451c3db1915fc653e77113d8
Abdul khaliq <99346275+Abdul5786@users.noreply.github.com>
2025-07-04 17:24:17 +0530

    Time: 6 ms (72.09%), Space: 45.3 MB (13.06%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
@@ -1,21 +1,27 @@
 class Solution {
     public int lengthOfLongestSubstring(String s) 
     {
-        int left=0 , maxLen=0;
-
-       HashMap<Character,Integer> seen =  new HashMap<>();
-
-        for(int right=0;right<s.length();right++)
+       Set<Character> window = new HashSet<>();
+       int right=0 , left=0; // pointers required to shifting
+       // check if already exist 
+       
+       int maxLen=0;
+       while(right<s.length())
+       {
+        
+        if(!window.contains(s.charAt(right)))
         {
-              char ch  =  s.charAt(right);
-
-              if(seen.containsKey(ch)&& seen.get(ch)>=left)
-              {
-                 left= seen.get(ch)+1;
-              }
-              seen.put(ch,right);
-              maxLen= Math.max(maxLen,right-left+1);
+            window.add(s.charAt(right));
+            maxLen = Math.max(maxLen,right-left+1);
+            right++;
         }
-        return maxLen;
+        
+        else {
+            window.remove(s.charAt(left));
+            left++;
+        }
+       }
+       
+       return maxLen;
     }
 }
