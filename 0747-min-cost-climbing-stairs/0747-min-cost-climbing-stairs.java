69c88e1a81b64106fd837a7dcaa95421a2161f4e
Abdul khaliq <99346275+Abdul5786@users.noreply.github.com>
2025-07-23 14:56:55 +0530

    Time: 0 ms (100%), Space: 43.3 MB (38%) - LeetHub

--- 0747-min-cost-climbing-stairs/0747-min-cost-climbing-stairs.java
+++ 0747-min-cost-climbing-stairs/0747-min-cost-climbing-stairs.java
@@ -1,29 +1,46 @@
 class Solution {
     public int minCostClimbingStairs(int[] cost) 
     {
-       int n =cost.length;
-       int dp[]= new int[n+1];
-       Arrays.fill(dp,-1);
-       return helper(n,cost,dp);
+        int dp[] = new int[cost.length];
+        int n= cost.length;
 
-    }
+         dp[0] = cost[0];
+         dp[1] =cost[1];
 
-    int helper(int n, int cost[],int dp[])
-    {
-        if(n==0 || n==1)
+        for(int i=2;i<n;i++)
         {
-            dp[n]=0;
-            return 0;
+            dp[i] = cost[i] + Math.min(dp[i-1],dp[i-2]);
         }
 
-        if(dp[n]!=-1)
-        {
-            return dp[n];
-        }
-        int oneStep = cost[n-1]+helper(n-1,cost,dp);
-        int twoStep =cost[n-2]+helper(n-2,cost,dp);
-
-        dp[n] =  Math.min(oneStep,twoStep); 
-        return dp[n];
+        return Math.min(dp[n-1],dp[n-2]);
     }
 }
+
+// class Solution {
+//     public int minCostClimbingStairs(int[] cost) 
+    
+//        int dp[]= new int[n+1];
+//        Arrays.fill(dp,-1);
+//        return helper(n,cost,dp);
+
+//     }
+
+//     int helper(int n, int cost[],int dp[])
+//     {
+//         if(n==0 || n==1)
+//         {
+//             dp[n]=0;
+//             return 0;
+//         }
+
+//         if(dp[n]!=-1)
+//         {
+//             return dp[n];
+//         }
+//         int oneStep = cost[n-1]+helper(n-1,cost,dp);
+//         int twoStep = cost[n-2]+helper(n-2,cost,dp);
+
+//         dp[n] =  Math.min(oneStep,twoStep); 
+//         return dp[n];
+//     }
+// }
