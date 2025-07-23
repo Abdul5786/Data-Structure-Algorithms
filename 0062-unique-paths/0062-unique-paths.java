b6be6054d0a34b94b68af0e3b077f5b9b514659a
Abdul khaliq <99346275+Abdul5786@users.noreply.github.com>
2025-07-23 15:09:38 +0530

    Time: 0 ms (100%), Space: 40.6 MB (35.8%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.java
+++ 0062-unique-paths/0062-unique-paths.java
@@ -1,27 +1,46 @@
 class Solution {
     public int uniquePaths(int m, int n) 
     {
-      int dp[][] =   new int[m+1][n+1];
-      for(int i=0;i<=m;i++)
-      {
-        for(int j=0;j<=n;j++)
+       int dp[][] = new int[m][n];
+
+       for(int i=0;i<m;i++)
+       {
+        for(int j=0;j<n;j++)
         {
-            dp[i][j]= -1;
+            if(i==0 || j==0) dp[i][j]=1;
+            else dp[i][j] = dp[i-1][j]+dp[i][j-1];
         }
-      }
+       }
 
-      return paths(m,n,dp);  
-    }
-
-    public int paths(int m, int n,int dp[][]) 
-    {
-        // unique path point
-       if(m==1 && n==1) return 1; // 1 path mil gyaa
-
-       if(m==0 || n==0) return 0; // invalid path 
-
-       if(dp[m][n]!=-1) return dp[m][n];
-       return dp[m][n] = paths(m,n-1,dp)+paths(m-1,n,dp);
-           
+       return dp[m-1][n-1]; 
     }
 }
+
+// class Solution {
+//     public int minCostClimbingStairs(int[] cost) 
+    
+//        int dp[]= new int[n+1];
+//        Arrays.fill(dp,-1);
+//        return helper(n,cost,dp);
+
+//     }
+
+//     int helper(int n, int cost[],int dp[])
+//     {
+//         if(n==0 || n==1)
+//         {
+//             dp[n]=0;
+//             return 0;
+//         }
+
+//         if(dp[n]!=-1)
+//         {
+//             return dp[n];
+//         }
+//         int oneStep = cost[n-1]+helper(n-1,cost,dp);
+//         int twoStep = cost[n-2]+helper(n-2,cost,dp);
+
+//         dp[n] =  Math.min(oneStep,twoStep); 
+//         return dp[n];
+//     }
+// }
