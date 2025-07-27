5a33715a8af0a76a17a1e1600a5552fbfd1f8aa5
Abdul khaliq <99346275+Abdul5786@users.noreply.github.com>
2025-07-27 21:43:33 +0530

    Time: 1 ms (99.69%), Space: 45.2 MB (59.64%) - LeetHub

--- 0412-fizz-buzz/0412-fizz-buzz.java
+++ 0412-fizz-buzz/0412-fizz-buzz.java
@@ -1,15 +1,29 @@
 class Solution {
-    public List<String> fizzBuzz(int n) {
-        List ans = new ArrayList<>();
-        for(int i = 1; i <= n; i++){
-            ans.add(
-                i % 15 == 0 ? "FizzBuzz" :
-                i % 5 == 0  ? "Buzz" :
-                i % 3 == 0  ? "Fizz" :
-                String.valueOf(i)
-            );
+    public List<String> fizzBuzz(int n) 
+    {
+       List<String> list =  new ArrayList<>();
+
+      for(int i=1;i<=n;i++)
+      {
+        if(i%15==0)
+        {
+            list.add("FizzBuzz");
+        }
+        else if(i%3==0)
+        {
+            list.add("Fizz");
+        }
+        else if(i%5==0)
+        {
+            list.add("Buzz");
         }
 
-        return ans;
+        else 
+        {
+            list.add(String.valueOf(i));
+        }
+      }
+
+      return list;
     }
-    }
+}
