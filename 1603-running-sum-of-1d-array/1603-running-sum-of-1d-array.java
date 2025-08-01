abaac8688d1b174a31923f5cd7bfa8843eb78bc2
Abdul khaliq <99346275+Abdul5786@users.noreply.github.com>
2025-08-01 21:55:17 +0530

    Time: 0 ms (100%), Space: 42.7 MB (76.72%) - LeetHub

--- 1603-running-sum-of-1d-array/1603-running-sum-of-1d-array.java
+++ 1603-running-sum-of-1d-array/1603-running-sum-of-1d-array.java
@@ -1,10 +1,16 @@
 class Solution {
     public int[] runningSum(int[] nums) 
     {
-        for(int i=1;i<nums.length;i++)
-        {
-            nums[i]=nums[i-1]+nums[i];
-        }
-         return nums;
+       
+       int prefix=0;
+
+       for(int i=0;i<nums.length;i++)
+       {
+           
+           prefix +=nums[i];
+           nums[i] =prefix;
+       }
+        
+        return nums;
     }
 }
