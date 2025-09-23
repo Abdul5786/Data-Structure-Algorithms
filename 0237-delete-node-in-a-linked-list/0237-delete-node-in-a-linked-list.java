9a8f95bbf0e28df6eea61279c1124498301e24d9
Abdul khaliq <99346275+Abdul5786@users.noreply.github.com>
2025-09-23 22:16:06 +0530

    Time: 0 ms (100%), Space: 44 MB (66.78%) - LeetHub

--- 0237-delete-node-in-a-linked-list/0237-delete-node-in-a-linked-list.java
+++ 0237-delete-node-in-a-linked-list/0237-delete-node-in-a-linked-list.java
@@ -1,17 +1,9 @@
-/**
- * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode(int x) { val = x; }
- * }
- */
 class Solution {
-    public void deleteNode(ListNode node) 
-    {
-        ListNode nextNode =node.next;
-        node.val=nextNode.val;
-        node.next=nextNode.next;
-        nextNode.next=null;
+    public void deleteNode(ListNode node) {
+        // Step 1: Copy the value of the next node into the current node
+        node.val = node.next.val;
+
+        // Step 2: Make the current node point to the next-to-next node
+        node.next = node.next.next;
     }
 }
