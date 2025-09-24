be34d3e1f1f1da9088ae9e363591cba90c43219d
Abdul khaliq <99346275+Abdul5786@users.noreply.github.com>
2025-09-24 22:43:06 +0530

    Time: 61 ms (78.65%), Space: 63.2 MB (14.54%) - LeetHub

--- 0739-daily-temperatures/0739-daily-temperatures.java
+++ 0739-daily-temperatures/0739-daily-temperatures.java
@@ -1,22 +1,19 @@
 class Solution {
     public int[] dailyTemperatures(int[] temperatures) 
     {
-       int results[] = new int [temperatures.length];
-       
-     Stack<Integer>  stack =  new Stack<>();
-     
-     for(int i=0;i<temperatures.length;i++)
-     {
-        
-        while(!stack.isEmpty() && temperatures[stack.peek()] <temperatures[i])
-        {
-            results[stack.peek()] = i-stack.pop();
-        }
-        
-        stack.push(i);
-        
-     }
-     
-     return results;
+       Stack<Integer> stack  =   new Stack<>();
+       int res[] = new int[temperatures.length];
+
+       for(int i=0;i<temperatures.length;i++)
+       {
+           while(!stack.isEmpty() && temperatures[i]>temperatures[stack.peek()])
+           {
+               int index = stack.pop();
+               res[index] = i-index;
+           }
+           stack.push(i);
+       }
+
+       return  res;   
     }
 }
