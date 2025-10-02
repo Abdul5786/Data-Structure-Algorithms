ca34a3c942996b8d22115396dc896c7143cb3337
Abdul khaliq <99346275+Abdul5786@users.noreply.github.com>
2025-10-02 20:32:32 +0530

    Time: 2 ms (52.71%), Space: 46.1 MB (73.7%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
@@ -1,33 +1,54 @@
 class Solution {
-   public double findMedianSortedArrays(int[] nums1, int[] nums2) {
-    int n1 = nums1.length;
-    int n2 = nums2.length;
+    public double findMedianSortedArrays(int[] nums1, int[] nums2) 
+    {
 
-    List<Integer> mergedArray = new ArrayList<>();
-    int i = 0, j = 0;
+      int n1= nums1.length;
+      int n2=nums2.length;
 
-    while (i < n1 && j < n2) {
-        if (nums1[i] < nums2[j]) {
-            mergedArray.add(nums1[i++]);
-        } else {
-            mergedArray.add(nums2[j++]);
+      int i=0, j=0;
+
+     ArrayList<Integer> res =  new ArrayList<>();
+
+
+      while(i<n1 && j<n2)
+      {
+
+         if(nums1[i]<nums2[j])
+         {
+         
+            res.add(nums1[i++]);
+         } 
+
+        else
+        {
+           res.add(nums2[j++]);
         }
-    }
 
-    while (i < n1) {
-        mergedArray.add(nums1[i++]);
-    }
+      }
 
-    while (j < n2) {
-        mergedArray.add(nums2[j++]);
-    }
+      while(i<n1)
+      {
+          res.add(nums1[i++]);
+      }
 
-    int n = n1 + n2;
-    if (n % 2 == 1) {
-        return mergedArray.get(n / 2);
-    } else {
-        return (mergedArray.get(n / 2 - 1) + mergedArray.get(n / 2)) / 2.0;
-    }
-}
+      while(j<n2)
+      {
+         res.add(nums2[j++]);
+      }
 
+
+     int n= n1+n2;
+      
+       if(n%2==1)  // odd
+       {
+           return res.get(n/2);
+       }
+
+
+        else
+        {
+            return (res.get(n/2-1)+res.get(n/2))/2.0;
+        }   
+
+    }
 }
