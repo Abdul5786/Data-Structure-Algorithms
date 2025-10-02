3ef7c15521fa39fbd89636c9a86e6842e4efdec0
Abdul khaliq <99346275+Abdul5786@users.noreply.github.com>
2025-10-02 22:59:15 +0530

    Time: 26 ms (20.6%), Space: 44.9 MB (89.93%) - LeetHub

--- 0907-koko-eating-bananas/0907-koko-eating-bananas.java
+++ 0907-koko-eating-bananas/0907-koko-eating-bananas.java
@@ -1,53 +1,53 @@
 class Solution {
     public int minEatingSpeed(int[] piles, int h) 
     {
-        int low=1;
-        int high= getMaxPiles(piles);
-        int result=high;
-        int ans=-1;
-
-       while(low<=high)
-       {
-                int mid= low+(high-low)/2;
-                if(canEatAllPiles(piles,h,mid))
-                {
-                    ans=mid;
-                    // check for minimum value again
-                    high=mid-1;
-                }
-
-                else
-                {
-
-                    low=mid+1;
-                }
-       }
+        int low=1; // minimum eating speed of  koko 
+        int high=getMaxPiles(piles);
+        int ans=0;
 
 
-      return ans;
+        while(low<=high)
+        {
+            int mid = low+(high-low)/2;
+
+            if(iscanEatAllBananas(piles,h,mid))
+            {
+                ans = mid; 
+                // try for more minimum value
+                high= mid-1;
+            }
+
+            else 
+            {
+                low =mid+1;
+            }
+        }
+
+       return ans;
     }
 
     public int getMaxPiles(int piles[])
     {
-       int maxPile=0;
-        for(int pi:piles)
-        {
-            maxPile=Math.max(pi,maxPile);
-        }
-        return maxPile;
-    }
+      
+        int max=Integer.MIN_VALUE;
 
-    // check if koko can eat all banas at given times
-
-    private boolean canEatAllPiles(int piles[],int h,int speed)
-    {
-
-        int totalHours=0;
         for(int pile:piles)
         {
-              totalHours+= Math.ceil((double) pile/speed);
+         max  = Math.max(max,pile);
         }
 
-        return totalHours<=h;
+        return max;
+    }
+
+    public boolean iscanEatAllBananas(int piles[], int time,int speed)
+    {
+        int totalHours=0;
+
+        for(int pile:piles)
+        {
+             totalHours  +=Math.ceil((double)pile/speed);
+        }
+        
+        return  totalHours<=time;
     }
 }
