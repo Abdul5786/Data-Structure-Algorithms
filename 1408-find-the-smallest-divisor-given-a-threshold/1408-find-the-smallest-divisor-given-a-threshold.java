fd3c88c3c83e448e518cf93a1b92996d7b419b99
Abdul khaliq <99346275+Abdul5786@users.noreply.github.com>
2025-10-03 15:54:03 +0530

    Time: 30 ms (32.57%), Space: 48 MB (81.34%) - LeetHub

--- 1408-find-the-smallest-divisor-given-a-threshold/1408-find-the-smallest-divisor-given-a-threshold.java
+++ 1408-find-the-smallest-divisor-given-a-threshold/1408-find-the-smallest-divisor-given-a-threshold.java
@@ -1,35 +1,59 @@
 class Solution {
-    public int smallestDivisor(int[] nums, int threshold) {
-        int low = 1;
-        int high = getMax(nums);
-        int ans = 0;
+    public int smallestDivisor(int[] nums, int threshold) 
+    {
+        int low=1;
+        int high= getMax(nums);
+        int ans=0;
+        
+        
+        
 
-        while (low <= high) {
-            int mid = low + (high - low) / 2;
+        while(low<=high)
+        {
+            int mid = low+(high-low)/2;
+            
+            if(isPossible(nums,mid,threshold))
+            {
+               ans = mid;
 
-            if (isPossible(nums, mid, threshold)) {
-                ans = mid;
-                high = mid - 1;
-            } else {
-                low = mid + 1;
+               // try for minimum 
+
+               high= mid-1;
             }
+
+            else 
+            {
+                 low = mid+1;
+            }
+
+           
         }
-        return ans;
+         return ans;
     }
 
-    private int getMax(int[] nums) {
-        int max = nums[0];
-        for (int n : nums) {
-            max = Math.max(max, n);
+    public int getMax(int nums[])
+    {
+        int max=0;
+        for(int num:nums)
+        {
+           max = Math.max(max,num);  
         }
+
         return max;
     }
+    
 
-    private boolean isPossible(int[] nums, int divisor, int threshold) {
-        long totalSum = 0;
-        for (int num : nums) {
+    public boolean isPossible(int nums[],int divisor, int threshold)
+    {
+
+        int totalSum =0;
+
+        for(int num:nums)
+        {
             totalSum += Math.ceil((double)num/divisor);
         }
-        return totalSum <= threshold;
+        
+        return totalSum<=threshold;
     }
+    
 }
