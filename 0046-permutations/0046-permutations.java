2f42800af7dc9b841ef3181984818b3f5ff3cd63
Abdul khaliq <99346275+Abdul5786@users.noreply.github.com>
2025-10-04 13:51:00 +0530

    Time: 1 ms (92.88%), Space: 44.7 MB (46.08%) - LeetHub

--- 0046-permutations/0046-permutations.java
+++ 0046-permutations/0046-permutations.java
@@ -1,28 +1,33 @@
 class Solution {
-    List<List<Integer>> ans = new ArrayList<>();
+
+    List<List<Integer>> ans  = new ArrayList<>();
     public List<List<Integer>> permute(int[] nums) 
     {
-     helper(nums, new ArrayList<>());
-     return ans;
+        helper(nums,new ArrayList<>());
+        return ans;
     }
 
-    private void helper(int[] arr, List<Integer> used) {
-    if (used.size() == arr.length) {  // Base case
-        ans.add(new ArrayList<>(used));
-        return;
-    }
 
-    for (int i = 0; i < arr.length; i++) {
-        if (!used.contains(arr[i])) {  // If condition
-            used.add(arr[i]);          // Add element
-            helper(arr, used);         // Recursive call
-            used.remove(used.size() - 1); // Backtrack
+    public void helper(int nums[],List<Integer> used)
+    {
+
+        // base case 
+
+        if(used.size()==nums.length)
+        {
+            ans.add(new ArrayList<>(used));
+            return;
         }
 
+        for(int i=0;i<nums.length;i++)
+        {
+            if(!used.contains(nums[i]))
+            {
+                used.add(nums[i]);
+                helper(nums,used);
+                // back track
+                used.remove(used.size()-1);
+            }
+        }
     }
-
-    }
-
 }
-
-    
