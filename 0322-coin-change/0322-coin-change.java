efa293c3abb159ed4aae77e60e6d69750b996a87
Abdul khaliq <99346275+Abdul5786@users.noreply.github.com>
2025-10-21 21:56:30 +0530

    Time: 10 ms (99.58%), Space: 44.2 MB (90.49%) - LeetHub

--- 0322-coin-change/0322-coin-change.java
+++ 0322-coin-change/0322-coin-change.java
@@ -1,21 +1,13 @@
 class Solution {
     public int coinChange(int[] coins, int amount) {
-        Integer[] memo = new Integer[amount + 1];
-        return helper(coins, amount, memo);
-    }
-    
-    private int helper(int[] coins, int amount, Integer[] memo) {
-        if (amount == 0) return 0;
-        if (amount < 0) return -1;
-        if (memo[amount] != null) return memo[amount];
-        
-        int min = Integer.MAX_VALUE;
-        for (int coin : coins) {
-            int res = helper(coins, amount - coin, memo);
-            if (res >= 0 && res < min) min = 1 + res;
+    int[] dp = new int[amount + 1];
+    Arrays.fill(dp, amount + 1);
+    dp[0] = 0;
+    for (int coin : coins) {
+        for (int i = coin; i <= amount; i++) {
+            dp[i] = Math.min(dp[i], dp[i - coin] + 1);
         }
-        
-        memo[amount] = (min == Integer.MAX_VALUE) ? -1 : min;
-        return memo[amount];
     }
+    return dp[amount] > amount ? -1 : dp[amount];
+}
 }
