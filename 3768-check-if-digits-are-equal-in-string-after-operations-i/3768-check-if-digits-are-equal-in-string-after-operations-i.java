ceec4d636697376ce547d734922da711f9a649e5
Abdul khaliq <99346275+Abdul5786@users.noreply.github.com>
2025-10-23 21:15:19 +0530

    Time: 2 ms (91.96%), Space: 44.5 MB (80.71%) - LeetHub

--- 3768-check-if-digits-are-equal-in-string-after-operations-i/3768-check-if-digits-are-equal-in-string-after-operations-i.java
+++ 3768-check-if-digits-are-equal-in-string-after-operations-i/3768-check-if-digits-are-equal-in-string-after-operations-i.java
@@ -1,22 +1,33 @@
 class Solution {
     public boolean hasSameDigits(String s) 
     {
-        if (s == null || s.length() < 2) return false; // no two digits to compare
+       
+         if(s==null || s.length()<2)  return false;
 
-        // convert string to int array of digits
-        int n = s.length();
-        int[] curr = new int[n];
-        for (int i = 0; i < n; i++) curr[i] = s.charAt(i) - '0';
+        // first i need to convert thi into int array to perform arthmetic operations
 
-        // reduce until length == 2
-        while (curr.length > 2) {
-            int[] next = new int[curr.length - 1];
-            for (int i = 0; i < curr.length - 1; i++) {
-                next[i] = (curr[i] + curr[i + 1]) % 10;
-            }
-            curr = next;
+        int curr[] = new int[s.length()]; 
+
+        for(int i=0;i<s.length();i++)
+        {
+            curr[i] = s.charAt(i)-'0';
         }
 
-        return curr.length == 2 && curr[0] == curr[1];
+
+        while(curr.length>2)
+        {
+
+            int next[] = new int[curr.length-1]; // it will  hold reduced array valuse
+
+            for(int i=0;i<curr.length-1;i++)
+            {
+                next[i] = (curr[i]+curr[i+1])%10;
+
+            }
+
+            curr= next; // now curr will point to next 
+        }
+
+        return curr[0]==curr[1] && curr.length==2;
     }
 }
