341c7ddf560901725289349ded6a685667f707e5
Abdul khaliq <99346275+Abdul5786@users.noreply.github.com>
2025-10-27 20:54:42 +0530

    Time: 0 ms (100%), Space: 40.4 MB (78.42%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.java
+++ 0062-unique-paths/0062-unique-paths.java
@@ -5,41 +5,17 @@ class Solution {
 
        for(int i=0;i<m;i++)
        {
-        for(int j=0;j<n;j++)
-        {
-            if(i==0 || j==0) dp[i][j]=1;
-            else dp[i][j] = dp[i-1][j]+dp[i][j-1];
-        }
+          for(int j=0;j<n;j++)
+          {
+
+             if(i==0 || j==0 ) dp[i][j]=1; 
+             else 
+             {
+                 dp[i][j] = dp[i-1][j]+dp[i][j-1];
+             }
+          }
        }
-       return dp[m-1][n-1]; 
+
+        return dp[m-1][n-1];
     }
 }
-
-// class Solution {
-//     public int minCostClimbingStairs(int[] cost) 
-    
-//        int dp[]= new int[n+1];
-//        Arrays.fill(dp,-1);
-//        return helper(n,cost,dp);
-
-//     }
-
-//     int helper(int n, int cost[],int dp[])
-//     {
-//         if(n==0 || n==1)
-//         {
-//             dp[n]=0;
-//             return 0;
-//         }
-
-//         if(dp[n]!=-1)
-//         {
-//             return dp[n];
-//         }
-//         int oneStep = cost[n-1]+helper(n-1,cost,dp);
-//         int twoStep = cost[n-2]+helper(n-2,cost,dp);
-
-//         dp[n] =  Math.min(oneStep,twoStep); 
-//         return dp[n];
-//     }
-// }
