8ee976fe657f1b0fa5bd7591eaf1485189ae5172
Abdul khaliq <99346275+Abdul5786@users.noreply.github.com>
2025-10-28 19:54:33 +0530

    Time: 12 ms (29.26%), Space: 44.7 MB (34.3%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,31 +1,37 @@
 class Solution {
     public boolean isAnagram(String s, String t) 
     {
-        
+           // we will create hashMap to count frquency of characters string
 
-             HashMap<Character,Integer>  map  =  new HashMap<>();
 
-             for(char ch : s.toCharArray())
-             {  
-                 map.put(ch,map.getOrDefault(ch,0)+1);
-             }
 
-             for(char ch :t.toCharArray())
+         // edge case
+
+         if(s.length()!=t.length()) return false;
+         HashMap<Character,Integer> map =   new HashMap<>();
+
+         for(char ch : s.toCharArray())
+         {
+
+            map.put(ch,map.getOrDefault(ch,0)+1);
+         }
+
+
+         for(char ch : t.toCharArray())
+         {
+
+            map.put(ch,map.getOrDefault(ch,0)-1);
+         }
+
+
+         for(int val :map.values())
+         {
+             if(val!=0)
              {
-                 map.put(ch,map.getOrDefault(ch,0)-1);
+                return false;
              }
+         }
 
-
-             for(int val : map.values())
-             {
-                if(val!=0)
-                {
-                     return false;
-                }
-             }
-
-
-             return true;
-
-    }    
+         return true;  
+    }
 }
