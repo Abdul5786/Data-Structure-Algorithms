edd2c4a0faa9ebcf102b99c6b11c8148af8c1925
Abdul khaliq <99346275+Abdul5786@users.noreply.github.com>
2025-10-30 02:08:51 +0530

    Time: 3 ms (88.22%), Space: 44.5 MB (27.69%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.java
+++ 0496-next-greater-element-i/0496-next-greater-element-i.java
@@ -5,16 +5,15 @@ class Solution {
 
        Stack<Integer> stack = new Stack<>();
 
-       for(Integer num : nums2)
+       for(int num : nums2)
        {
           while(!stack.isEmpty() && num>stack.peek())
           {
-             map.put(stack.pop(),num);
+               map.put(stack.pop(),num);
           }
 
           stack.push(num);
        }
-
        int res[] =  new int[nums1.length];
        for(int i=0;i<nums1.length;i++)
        {
